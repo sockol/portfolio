ee1122495d71ef0adae57f53cd057e2f452af01c
Semur Nabiev <semur.nabiev@ibm.com>
2019-07-22 19:33:37 -0500

    heap

--- pages/_document.js
+++ pages/_document.js
@@ -46,6 +46,14 @@ export default class MyDocument extends Document {
 
           {this.props.styleTags}
 
+          {
+            process.env.NODE_ENV === `production` &&
+            <script type="text/javascript">{` 
+              window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
+              heap.load("1475914233"); 
+            `}</script>
+          }
+
           <link rel="apple-touch-icon" sizes="57x57" href="/static/apple-icon-57x57.png"/>
           <link rel="apple-touch-icon" sizes="60x60" href="/static/apple-icon-60x60.png"/>
           <link rel="apple-touch-icon" sizes="72x72" href="/static/apple-icon-72x72.png"/>
